5b62dc2f46d318700cbae7252b0e4d30fdd8fd8b
Rathan Kumar <rathan.kumar492@gmail.com>
2021-03-16 16:19:03 +0530

    product order and payments

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
     'store',
     'carts',
+    'orders',
 ]
 
 MIDDLEWARE = [
--- greatkart/urls.py
+++ greatkart/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
+
+    # ORDERS
+    path('orders/', include('orders.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
